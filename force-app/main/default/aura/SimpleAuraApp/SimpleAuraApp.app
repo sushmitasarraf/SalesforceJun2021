eaf30a0278600ee33b30d26da95ed3e126687430
sushmitasarraf <sushnita.sarraf123@gmail.com>
2021-08-11 13:58:36 +0530

    Simple Aura Component

--- force-app/main/default/aura/SimpleAuraApp/SimpleAuraApp.app
+++ force-app/main/default/aura/SimpleAuraApp/SimpleAuraApp.app
@@ -1,3 +1,3 @@
 <aura:application>
-    <c:SimpleAuroCpmponent />
+    <c:SimpleAuraComponent />
 </aura:application>	
--- force-app/main/default/aura/SimpleAuraComponent/SimpleAuroCpmponentController.js
+++ force-app/main/default/aura/SimpleAuraComponent/SimpleAuroCpmponentController.js
@@ -1,5 +1,12 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        console.log('Came into doInit');
+        console.log('person: ' + component.get("v.person"));
+        component.set("v.person", 'Have Fun Solving');
+    },
+    sayHello : function(cmp, event, helper) {
+        console.log('Came into sayHello');
+        const name = cmp.find("nameInputId").get("v.value"); //Get input text field value
+        cmp.set("v.person", name); //Set the component attribute value
+    } 
 })
